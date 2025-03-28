c45b06bbf0238b324eabedcb9a929a10e4955db5
James Leone <jsalleo13@gmail.com>
2025-03-28 00:57:15 -0400

    Remove comment from deleteSongsInCloud Module

--- query/modules/deleteSongsInCloud.js
+++ query/modules/deleteSongsInCloud.js
@@ -68,27 +68,4 @@ export const deleteSongsInCloud = async (
 
     return;
   }
-
-  // // Deleting the s3 object from the s3 bucket
-  // for (let song of songs) {
-  //   s3Params["Key"] = `${song["audio"]}`;
-  //   const musicCommand = new DeleteObjectCommand(s3Params);
-  //   await s3Client.send(musicCommand);
-
-  //   s3Params["Key"] = `${song["image_art"]}`;
-  //   const imageCommand = new DeleteObjectCommand(s3Params);
-  //   await s3Client.send(imageCommand);
-
-  //   // Invalidating the music data in the
-  //   // CloudFront cache
-  //   cloudFrontParams["InvalidationBatch"]["Paths"]["Items"] = [
-  //     "/" + `${song["audio"]}`,
-  //     "/" + `${song["image_art"]}`,
-  //   ];
-
-  //   const cloudFrontSongCommand = new CreateInvalidationCommand(
-  //     cloudFrontParams
-  //   );
-  //   await cloudFrontClient.send(cloudFrontSongCommand);
-  // }
 };
